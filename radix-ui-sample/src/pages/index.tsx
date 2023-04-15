039e3f2195484ee0a92481eac7dffb49bbf4b799
Kaito Iwatsuki <kaito2.iwatsuki@gmail.com>
2023-04-16 01:13:13 +0900

    Radix UI (#1)
    * create-next-app
    
    * Toolbar
    
    * tailwindcss-radix

---
+++ radix-ui-sample/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import { Accordion } from "@/feature/popover/Accordion";
+import ToolbarDemo from "@/feature/popover/ToolbarDemo";
+
+export default function Home() {
+  return (
+    <>
+      <ToolbarDemo />
+      <Accordion />
+    </>
+  );
+}
