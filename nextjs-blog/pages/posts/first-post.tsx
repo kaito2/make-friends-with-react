9d66ff53ec21cf214eb16fcddba7092cba6a3d7a
kaito2 <kaito2.iwatsuki@gmail.com>
2023-05-14 18:29:06 +0900

    done

--- nextjs-blog/pages/posts/first-post.tsx
+++ nextjs-blog/pages/posts/first-post.tsx
@@ -1,13 +1,20 @@
+import Head from "next/head";
 import Link from "next/link";
 import { FC } from "react";
+import Layout from "../../components/layout";
 
 const FirstPost: FC = () => {
   return (
     <>
-      <h1>最初の投稿</h1>
-      <h2>
-        <Link href="/">Back to home</Link>
-      </h2>
+      <Layout>
+        <Head>
+          <title>First Post</title>
+        </Head>
+        <h1>最初の投稿</h1>
+        <h2>
+          <Link href="/">Back to home</Link>
+        </h2>
+      </Layout>
     </>
   );
 };
